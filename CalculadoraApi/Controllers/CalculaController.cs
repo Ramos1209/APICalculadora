1a28bd131ac969101c0f4117af53d94f95a3c498
Paulo Cesar Ramos <paulo.1209@terra.com.br>
2019-12-06 08:27:51 -0300

    Add project files.

---
+++ CalculadoraApi/Controllers/CalculaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculadoraApi.Entity;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraApi.Controllers
+{
+    [EnableCors("AllowSpecificOrigin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculaController : ControllerBase
+    {
+
+        [HttpPost]
+        public ActionResult<Calcula> Post([FromBodyAttribute] Calcula calcula)
+        {
+            var c = new Calcula();
+            c.Numero1 = calcula.Numero1;
+            c.Numero2 = calcula.Numero2;
+            c.TipoOperacao = calcula.TipoOperacao;
+            c.Resultado = calcula.Resultado;
+            c.Calculado(c);
+            return c;
+           
+        }
+    }
+}
---
+++ CalculadoraApi/Entity/Calcula.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace CalculadoraApi.Entity
+{
+    public  class Calcula
+    {
+        public int Numero1 { get; set; }
+       public int Numero2 { get; set; }
+       public  int Resultado { get; set; }
+        public string TipoOperacao { get; set; }
+
+          
+        public int Calculado(Calcula calcula)
+        {
+            switch (TipoOperacao)
+            {
+                case "A":
+                    {
+                        Resultado = Numero1 + Numero2;
+                        break;
+                    }
+                case "S":
+                    {
+                        Resultado = Numero1 - Numero2;
+
+                        break;
+                    }
+                case "M":
+                    {
+                        Resultado = Numero1 * Numero2;
+                        break;
+                    }
+                case "D":
+                    {
+                        Resultado = Numero1 / Numero2;
+                        break;
+                    }
+            }
+            return Resultado;
+
+        }
+
+    }
+}
+
